ca8a67297d9763ce68308299964825685fff8d09
435421355@qq.com <wenlong_chen@sui.com>
2022-01-01 19:38:06 +0800

    新博客的大概构建

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -8,15 +8,41 @@ module.exports = {
       }
     }
   },
+  locales: {
+    '/': {
+      lang: 'zh-CN'
+    }
+  },
   themeConfig: {
-      nav: [
-          { text: '首页', link: '/' },
-          { 
-              text: 'LeslieC的 JavaScript 博客', 
-              items: [
-                  { text: 'Github', link: 'https://github.com/chenwenlong7' }
-              ]
-          }
-      ]
+    base: '/study',
+    subSidebar: 'auto',
+    nav: [
+      { text: '首页', link: '/' },
+      { 
+        text: 'LeslieC的 JavaScript 博客', 
+        items: [
+          { text: 'Github', link: 'https://github.com/chenwenlong7' }
+        ]
+      }
+    ],
+    sidebar: [
+      {
+          title: '欢迎学习',
+          path: '/',
+          collapsable: false, // 不折叠
+          children: [
+              { title: "学前必读", path: "/" }
+          ]
+      },
+      {
+        title: "基础学习",
+        path: '/handbook/ConditionalTypes',
+        collapsable: false, // 不折叠
+        children: [
+          { title: "条件类型", path: "/handbook/ConditionalTypes" },
+          { title: "泛型", path: "/handbook/Generics" }
+        ],
+      }
+    ]
   }
 }
