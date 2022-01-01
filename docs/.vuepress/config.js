4fdc3a38df454d53675a187a5bfc94d5de8b9fd3
wenlong_chen <wenlong_chen@sui.com>
2022-01-01 19:03:53 +0800

    反复测试

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -14,8 +14,7 @@ module.exports = {
           { 
               text: 'LeslieC的 JavaScript 博客', 
               items: [
-                  { text: 'Github', link: 'https://github.com/chenwenlong7' },
-                  { text: '777', link: 'https://github.com/chenwenlong7' }
+                  { text: 'Github', link: 'https://github.com/chenwenlong7' }
               ]
           }
       ]
