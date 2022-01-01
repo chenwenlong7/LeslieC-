fee6646eff66f7355b0e656aff723d38d364aff1
wenlong_chen <wenlong_chen@sui.com>
2022-01-01 17:59:34 +0800

    项目上传代码

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,22 @@
+module.exports = {
+  title: 'leslieC的学习笔记',
+  description: '虽然我卷，但我还是菜啊',
+  configureWebpack: {
+      resolve: {
+        alias: {
+        '@alias': 'path/to/some/dir'
+      }
+    }
+  },
+  themeConfig: {
+      nav: [
+          { text: '首页', link: '/' },
+          { 
+              text: 'LeslieC的 JavaScript 博客', 
+              items: [
+                  { text: 'Github', link: 'https://github.com/mqyqingfeng' }
+              ]
+          }
+      ]
+  }
+}
